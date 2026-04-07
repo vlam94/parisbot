665d0a14077166d6238e02a7cc7156b6a2b9ccf8
Victor Mello <vlam94@gmail.com>
2026-04-07 17:13:53 -0300

    updating table creation scripts

--- data-pipeline/pipeline/src/main/sql/tables.sql
+++ data-pipeline/pipeline/src/main/sql/tables.sql
@@ -1,6 +1,7 @@
+-- METEOBLUE CURRENT TASK 
 CREATE TABLE IF NOT EXISTS meteoblue.current_fact (
     requested_at TIMESTAMPTZ,
-    api_system_time TIMESTAMPTZ,
+    weather_data_time TIMESTAMPTZ,
     isobserveddata BOOLEAN,
     metarid VARCHAR(50),
     windspeed NUMERIC,
@@ -14,3 +15,20 @@ CREATE TABLE IF NOT EXISTS meteoblue.current_fact (
     response_generation_ms NUMERIC
 );
 ALTER TABLE meteoblue.current_fact OWNER TO datawarehouse;
+
+-- METEOBLUE FORECAST TASK
+CREATE TABLE IF NOT EXISTS meteoblue.forecast_fact (
+    requested_at TIMESTAMPTZ,
+    forecast_time TIMESTAMPTZ,
+    windspeed NUMERIC,
+    temperature NUMERIC,
+    precipitation_probability NUMERIC,
+    rainspot NUMERIC,
+    pictocode SMALLINT,
+    felttemperature NUMERIC,
+    precipitation NUMERIC,
+    relativehumidity NUMERIC,
+    winddirection NUMERIC,
+    geo_coordinates VARCHAR
+);
+ALTER TABLE meteoblue.forecast_fact OWNER TO datawarehouse;
