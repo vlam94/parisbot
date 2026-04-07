510a3aba579455e048f4a7f260187906890fdbed
Victor Mello <vlam94@gmail.com>
2026-04-07 17:12:48 -0300

    fixing forecast data transform function

--- data-pipeline/pipeline/src/main/python/dags/meteoblue.py
+++ data-pipeline/pipeline/src/main/python/dags/meteoblue.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from os import environ as env
-from tools.transformations.time import localize_with_numeric_offset_hours
+from datetime import timedelta
+from tools.transformations.time import convert_utc_timestamp_with_offset_hours, localize_timestamp_with_offset_hours
 from tools.transformations.generic import int_to_bool
 from tools.operators.postgres import RequestToPostgresOperator
 from airflow.sdk import DAG
@@ -12,7 +13,7 @@ target_table_forecast = "meteoblue.forecast_fact"
 
 target_columns_current = {
     "requested_at" :"timestamptz",
-    "api_system_time" : "timestamptz",
+    "weather_data_time" : "timestamptz",
     "isobserveddata" : "boolean",
     "metarid" : "varchar",
     "windspeed" : "numeric",
@@ -20,19 +21,25 @@ target_columns_current = {
     "pictocode_detailed" :"smallint",
     "pictocode" :"smallint",
     "temperature" :"numeric",
-    "latitude" :"numeric",
-    "longitude" :"numeric",
+    "geo_coordinates" :"varchar",
     "last_api_update" :"timestamptz",
     "response_generation_ms" :"numeric",
 }
 
 target_columns_forecast = {
-
+    "requested_at": "timestamptz",
+    "forecast_time": "timestamptz",
+    "windspeed": "numeric",
+    "temperature": "numeric",
+    "precipitation_probability": "numeric",
+    "rainspot": "numeric",
+    "pictocode": "smallint",
+    "felttemperature": "numeric",
+    "precipitation": "numeric",
+    "relativehumidity": "numeric",
+    "winddirection": "numeric",
+    "geo_coordinates": "varchar",
 }
-
-api_key = env.get('METEO_BLUE_API_KEY')
-lat, lon = -22.934, -43.180
-url = f"https://my.meteoblue.com/packages/current_basic-1h_basic-day?lat={lat}&lon={lon}&apikey={api_key}&forecast_days=1"
 
 def transform_current(data: pd.DataFrame, params: dict = {}) -> pd.DataFrame:
 
@@ -42,23 +49,27 @@ def transform_current(data: pd.DataFrame, params: dict = {}) -> pd.DataFrame:
 
     # Localizing timestamp based on request metadata
     data_current["last_api_update"] = data_current.apply(
-    lambda row: localize_with_numeric_offset_hours(row["modelrun_updatetime_utc"], row["utc_timeoffset"]),
+    lambda row: convert_utc_timestamp_with_offset_hours(row["modelrun_updatetime_utc"], row["utc_timeoffset"]),
     axis=1
     )
     data_current["requested_at"] = data_current.apply(
-    lambda row: localize_with_numeric_offset_hours(row["modelrun_utc"], row["utc_timeoffset"]),
+    lambda row: convert_utc_timestamp_with_offset_hours(row["modelrun_utc"], row["utc_timeoffset"]),
+    axis=1
+    )
+    data_current["weather_data_time"] = data_current.apply(
+    lambda row: localize_timestamp_with_offset_hours(row["time"], row["utc_timeoffset"]),
     axis=1
     )
 
-    #converting numeric boolean (0,1) to bool (True,False)
+    # Converting numeric boolean (0,1) to bool (True,False)
     data_current["isobserveddata"] = data_current["isobserveddata"].apply(int_to_bool)
 
     data_current = data_current.rename(columns={
         'generation_time_ms': 'response_generation_ms',
-        'time': 'api_system_time',
     })
 
-    print('\n'.join([f"column: {col}\ndata: {data_current[col].head()}\n" for col in data_current.columns]))
+    data_current['geo_coordinates'] = data_current['latitude'].astype(str) + ',' + data_current['longitude'].astype(str)
+
     return data_current
 
 
@@ -67,19 +78,31 @@ def transform_forecast(data: pd.DataFrame, params: dict = {}) -> pd.DataFrame:
     # Unpacking data from the nested JSON
     data_forecast = pd.json_normalize(data["data_1h"]).reset_index(drop=True)
     data_forecast = pd.concat([data_forecast, pd.json_normalize(data["metadata"])], axis=1)
-
-    # Localizing timestamp based on request metadata
     data_forecast = pd.json_normalize(data["data_1h"]).reset_index(drop=True)
+
     forecast_columns = data_forecast.columns.to_list()
+    # Converting timestamp based on request metadata 
     data_forecast = pd.concat([data_forecast, pd.json_normalize(data["metadata"])], axis=1)
     data_forecast["requested_at"] = data_forecast.apply(
-        lambda row: localize_with_numeric_offset_hours(row["modelrun_updatetime_utc"], row["utc_timeoffset"]),
+        lambda row: convert_utc_timestamp_with_offset_hours(row["modelrun_updatetime_utc"], row["utc_timeoffset"]),
         axis=1
     )
+
+    data_forecast['geo_coordinates'] = data_forecast['latitude'].astype(str) + ',' + data_forecast['longitude'].astype(str)
+
     data_forecast = data_forecast.explode(forecast_columns).reset_index(drop=True)
-    data_forecast
+
+    data_forecast["forecast_time"] = data_forecast.apply(
+    lambda row: localize_timestamp_with_offset_hours(row["time"], row["utc_timeoffset"]),
+    axis=1
+    )
+    data_forecast = data_forecast[data_forecast['forecast_time'] >= data_forecast['requested_at']].reset_index(drop=True)
 
     return data_forecast
+
+api_key = env.get('METEO_BLUE_API_KEY')
+lat, lon = -22.934, -43.180
+url = f"https://my.meteoblue.com/packages/current_basic-1h_basic-day?lat={lat}&lon={lon}&apikey={api_key}&forecast_days=1"
 
 with DAG(
     dag_id="meteoblue_request_to_dw",
@@ -94,7 +117,9 @@ with DAG(
         transform_function = transform_current,
         target_table = target_table_current,
         target_columns = target_columns_current,
-        cache_expire_seconds= cache_expire_seconds
+        cache_expire_seconds= cache_expire_seconds,
+        retries = 5,
+        retry_delay = timedelta(minutes=10)
     )
 
     forecast_task = RequestToPostgresOperator(
@@ -103,7 +128,9 @@ with DAG(
         transform_function = transform_forecast,
         target_table = target_table_forecast,
         target_columns = target_columns_forecast,
-        cache_expire_seconds= cache_expire_seconds
+        cache_expire_seconds= cache_expire_seconds,
+        retries = 5,
+        retry_delay = timedelta(minutes=20)
     )
 
     current_task >> forecast_task
